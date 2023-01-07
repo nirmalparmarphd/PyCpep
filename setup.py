7ce6168b6a89fe9047f9119b198ee7db20d97817
nirmalparmarphd <nirmalparmarphd@gmail.com>
2023-01-07 21:31:51 +0100

    pycpep 1.0.2 deployed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pycpep',
-        version='1.0.1', 
+        version='1.0.2', 
         description='Predicts deviation in the heat capacity measurement for microDSC Tian-Calvet', 
         author='Nirmal Parmar', 
         packages=['pycpep'],
